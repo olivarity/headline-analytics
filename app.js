e312dc4b4e3f36a92657b2265ac80198bee4bea5
Olivia Isaacs <olivia@scaasic.com>
2018-03-25 21:48:28 -0500

    Basic webpage

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('tiny'));
+
+app.use(express.static('public'));
 app.get('*', (req, res) => {
-  res.send('Hello World!');
+  res.sendStatus(404);
 });
 
 const port = process.env.PORT || 3000;
